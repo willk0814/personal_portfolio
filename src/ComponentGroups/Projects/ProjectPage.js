058161b9f8dc8ce5c0c535b948489de71d037bfa
willk0814 <wkoenig0814@gmail.com>
2023-03-13 15:08:55 -0400

    Sliders removed from project page

--- src/ComponentGroups/Projects/ProjectPage.js
+++ src/ComponentGroups/Projects/ProjectPage.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import AppDemo from "../Assets/DeviceControllerDemo.MP4";
-import sortingDemo from "../Assets/sortingVisualizer.MP4";
 
 import "../PageStyling/PageStyles.css";
 
@@ -30,145 +28,66 @@ export default function ProjectPage() {
     <div name="projects" className="pageContainer">
       <div className="contentContainer">
         <h1 className="titleText">Projects</h1>
-        {/* Project - Device Controller */}
-        <div
-          style={{
-            width: appExpanded ? "80vh" : "55vh",
-            height: appExpanded ? "70vh" : "5vh",
-          }}
-          className="expandable"
-          onMouseEnter={toggleApp}
-          onMouseLeave={toggleApp}
-        >
-          <p>Research Device Controller Application</p>
-          <div
-            style={{
-              display: appExpanded ? true : "none",
-            }}
-            className="expandableBox"
+        <div>
+          <p
+            style={{ display: "inline", marginRight: "9px" }}
+            className="subTitle"
           >
-            <div className="projectExpandable">
-              <div>
-                <p>
-                  This is the React Native application that I built while
-                  working at the Sparks Lab. It was used to connect to research
-                  devices, initiate tests via bluetooth, and recieve and process
-                  data.
-                </p>
-                <a href="https://github.com/willk0814/DeviceControllerApp">
-                  <button className="submitMessageButton">Repo</button>
-                </a>
-              </div>
-              <video
-                height={550}
-                autoPlay={true}
-                loop={true}
-                className="alignDemo"
-                muted
-              >
-                <source src={AppDemo} type="video/mp4" />
-              </video>
-            </div>
-          </div>
+            Device Controller Application
+          </p>
+          <a
+            href="https://github.com/willk0814/DeviceControllerApp"
+            target="_blank"
+          >
+            <button className="submitMessageButton">Repo</button>
+          </a>
+          <ul className="subList">
+            <li className="subListItem">
+              This is the mobile application that I developed while working at
+              the Sparks Lab. It was made to aid in their research process by
+              allowing researchers to interact with research devices via
+              bluetooth
+            </li>
+            <li className="subListItem">
+              This application was developed in JavaScript using React Native
+            </li>
+          </ul>
         </div>
 
-        {/* Project - Sorting Visualizer */}
-        <div
-          style={{
-            width: sortingExpanded ? "80vh" : "55vh",
-            height: sortingExpanded ? "27vh" : "5vh",
-          }}
-          className="expandable"
-          onMouseEnter={toggleSorting}
-          onMouseLeave={toggleSorting}
-        >
-          <p>Sorting Visualizer</p>
-          <div
-            style={{
-              display: sortingExpanded ? true : "none",
-            }}
-            className="expandableBox"
+        <div>
+          <p
+            style={{ display: "inline", marginRight: "9px" }}
+            className="subTitle"
           >
-            <div className="projectExpandable">
-              <div>
-                <p>
-                  A react project to demonstrate the comparisons made by
-                  different sorting algorithms when sorting an array
-                </p>
-                <a
-                  href="https://github.com/willk0814/sorting_visualizer"
-                  target="_blank"
-                >
-                  <button
-                    className="submitMessageButton"
-                    style={{ marginRight: "10px" }}
-                  >
-                    Repo
-                  </button>
-                </a>
-                <a
-                  href="https://willk0814.github.io/sorting_visualizer/"
-                  target="_blank"
-                >
-                  <button className="submitMessageButton">Site</button>
-                </a>
-              </div>
-              <video
-                width={285}
-                autoPlay={true}
-                loop={true}
-                className="alignDemo"
-                muted
-              >
-                <source src={sortingDemo} type="video/mp4" />
-              </video>
-            </div>
-          </div>
+            Sorting Visualizer
+          </p>
+          <a
+            href="https://github.com/willk0814/sorting_visualizer"
+            target="_blank"
+          >
+            <button
+              className="submitMessageButton"
+              style={{ marginRight: "9px" }}
+            >
+              Repo
+            </button>
+          </a>
+          <a
+            href="https://willk0814.github.io/sorting_visualizer/"
+            target="_blank"
+          >
+            <button className="submitMessageButton">Site</button>
+          </a>
+          <ul className="subList">
+            <li className="subListItem">
+              A website that I developed to demonstrate the comparisons made as
+              an array is sorted using different algorithms.
+            </li>
+            <li className="subListItem">
+              I did this project in JavaScript in React
+            </li>
+          </ul>
         </div>
-
-        {/* Project - 2048 Game */}
-        {/* <div 
-            style={{
-              width: pythonGameExpanded ? '80vh' : '55vh',
-              height: pythonGameExpanded ? '20vh' : '5vh'
-            }}
-            className='expandable'
-            onMouseEnter={togglePythonGame}
-            onMouseLeave={togglePythonGame}>
-            <p>Python 2048 Game</p>
-            <div 
-              style={{
-                display: pythonGameExpanded ? true : 'none'
-              }}
-              className='expandableBox'>
-              <p>accomplishments</p>
-              <p>accomplishments</p>
-              <p>accomplishments</p>
-              <p>accomplishments</p>
-            </div>
-          </div> */}
-
-        {/* Project - Path Finding */}
-        {/* <div 
-            style={{
-              width: pathFindingExpanded ? '80vh' : '55vh',
-              height: pathFindingExpanded ? '20vh' : '5vh'
-            }}
-            className='expandable'
-            onMouseEnter={togglePathFinding}
-            onMouseLeave={togglePathFinding}>
-            <p>Path Finding Visualizer</p>
-            <div 
-              style={{
-                display: pathFindingExpanded ? true : 'none',
-              }}
-              className='expandableBox'>
-              <div className='projectExpandable'>
-                <p>accomplishments</p>
-                <p>accomplishments</p>
-              </div>
-            </div>
-          </div> */}
       </div>
     </div>
   );
