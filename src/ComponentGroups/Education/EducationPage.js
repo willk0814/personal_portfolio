4396eff955a4250c3b4e8a2ee4916442062c8211
willk0814 <wkoenig0814@gmail.com>
2023-03-13 14:07:58 -0400

    Changes to education and experience pages

--- src/ComponentGroups/Education/EducationPage.js
+++ src/ComponentGroups/Education/EducationPage.js
@@ -31,121 +31,48 @@ export default function EducationPage() {
       <div className="contentContainer">
         <h1 className="titleText">Education & Skills</h1>
 
-        {/* Non Expandable Layout */}
+        {/* Non Expandable Layout - Education */}
         <div>
-          <p>B.S. in Computer Science concentrated in AI</p>
-          <p> CS Coursework:</p>
-          <p> Math Coursework:</p>
+          <p className="subTitle">
+            B.S. in CS concentrated in AI w/ minor in CyberSecurity
+          </p>
+          <ul className="subList">
+            <li className="subListItem">
+              <p className="subSectTitle">CS Coursework:</p> OOP, Data
+              Structures, Algorithms, AI, Machine Learning, Computer Vision,
+              Database Systems, Operating Systems, Parallel Computing, Data
+              Mining
+            </li>
+            <li className="subListItem">
+              <p className="subSectTitle">Math Coursework:</p> Calculus I & II,
+              Linear Algebra, Automata Theory, Programming Logic, Statistical
+              Methods
+            </li>
+            <li className="subListItem">
+              <p className="subSectTitle">CyberSecurity Coursework:</p> Cloud
+              Computing & Security, System Hardening, Computer Networks, Web App
+              Security
+            </li>
+            <li className="subListItem">
+              <p className="subSectTitle">Core Coursework GPA:</p>
+            </li>
+          </ul>
         </div>
 
-        {/* Major */}
-        <div
-          style={{
-            width: majorExpanded ? "80vh" : "60vh",
-            height: majorExpanded ? "25vh" : "5vh",
-          }}
-          className="expandable"
-          onMouseEnter={toggleMajor}
-          onMouseLeave={toggleMajor}
-        >
-          <p>B.S. in Computer Science concentrated in AI</p>
-          <div
-            style={{
-              display: majorExpanded ? true : "none",
-            }}
-            className="expandableBox"
-          >
-            <p>
-              CS Coursework: OOP, Data Structures, Algorithms, AI, Machine
-              Learning, Computer Vision, Database Systems, Operating Systems,
-              Parallel Computing, Data Mining
-            </p>
-            <p>
-              Math Coursework: Calculus I & II, Linear Algebra, Automata Theory,
-              Programming Logic, Statistical Methods
-            </p>
-            <p>Major Coursework GPA:</p>
-          </div>
-        </div>
-
-        {/* Minor */}
-        <div
-          style={{
-            width: minorExpanded ? "80vh" : "60vh",
-            height: minorExpanded ? "20vh" : "5vh",
-          }}
-          className="expandable"
-          onMouseEnter={toggleMinor}
-          onMouseLeave={toggleMinor}
-        >
-          <p>Minor in CyberSecurity</p>
-          <div
-            style={{
-              display: minorExpanded ? true : "none",
-            }}
-            className="expandableBox"
-          >
-            <p>
-              Coursework: Cloud Computing & Security, System Hardening, Computer
-              Networks, Web App Security
-            </p>
-            <p>Minor Coursework GPA:</p>
-          </div>
-        </div>
-
-        {/* Major */}
-        <div
-          style={{
-            width: extraCurricularExpanded ? "80vh" : "60vh",
-            height: extraCurricularExpanded ? "25vh" : "5vh",
-          }}
-          className="expandable"
-          onMouseEnter={toggleExtraCurricular}
-          onMouseLeave={toggleExtraCurricular}
-        >
-          <p>Extra Curricular Involvement: Business Basics for Engineers</p>
-          <div
-            style={{
-              display: extraCurricularExpanded ? true : "none",
-            }}
-            className="expandableBox"
-          >
-            <p>
-              Founded an academic student organization aimed at teaching myself
-              and my peers how to effectively market their STEM degrees in and
-              out of tech industries.
-            </p>
-            <p>
-              Organizaed meetings, recruited speakers from industry, and hosted
-              student led discussions over zoom
-            </p>
-            <p>Maintained a membership of 65 students throughout my tenure</p>
-          </div>
-        </div>
-
-        {/* Skills */}
-        <div
-          style={{
-            width: skillsExpanded ? "80vh" : "60vh",
-            height: skillsExpanded ? "20vh" : "5vh",
-          }}
-          className="expandable"
-          onMouseEnter={toggleSkills}
-          onMouseLeave={toggleSkills}
-        >
-          <p>Relevant Skills</p>
-          <div
-            style={{
-              display: skillsExpanded ? true : "none",
-            }}
-            className="expandableBox"
-          >
-            <p>
-              Languages and Frameworks: Python, Java, Javascript, C++, React,
-              React Native, HTML, CSS, NumPy, PyTorch, matplotlib, SQL
-            </p>
-            <p>Project Management Skills and Tools: Git, Jira, Asana</p>
-          </div>
+        {/* Non Expandable Layout -  Skills */}
+        <div>
+          <p className="subTitle">Relevant Skills</p>
+          <ul className="subList">
+            <li className="subListItem">
+              <p className="subSectTitle">Languages & Frameworks:</p> Python,
+              Java, Javascript, C++, React, React Native, HTML, CSS, NumPy,
+              PyTorch, matplotlib, SQL
+            </li>
+            <li className="subListItem">
+              <p className="subSectTitle">Project Management Tools:</p> Git,
+              Jira, Asana
+            </li>
+          </ul>
         </div>
       </div>
     </div>
--- src/ComponentGroups/WorkExperience/WorkExperiencePage.js
+++ src/ComponentGroups/WorkExperience/WorkExperiencePage.js
@@ -1,65 +1,68 @@
-import React, { useState } from 'react'
-import '../PageStyling/PageStyles.css'
+import React, { useState } from "react";
+import "../PageStyling/PageStyles.css";
 
 export default function WorkExperiencePage() {
-
-  const [viinkoExpanded, setViinkoExpanded] = useState(false)
-  const [sparksExpanded, setSparksExpanded] = useState(false)
+  const [viinkoExpanded, setViinkoExpanded] = useState(false);
+  const [sparksExpanded, setSparksExpanded] = useState(false);
 
   const toggleSparks = () => {
-    setSparksExpanded(!sparksExpanded)
-  }
+    setSparksExpanded(!sparksExpanded);
+  };
 
   const toggleViinko = () => {
-    setViinkoExpanded(!viinkoExpanded)
-  }
+    setViinkoExpanded(!viinkoExpanded);
+  };
 
   return (
-    <div name='experience' className='pageContainer'>
-      <div
-        className='contentContainer'>
-          <h1 className='titleText'>Work Experience</h1>
-
-          {/* Sparks Lab */}
-          <div 
-            style={{
-              width: sparksExpanded ? '80vh' : '55vh',
-              height: sparksExpanded ? '20vh' : '5vh'
-            }}
-            className='expandable'
-            onMouseEnter={toggleSparks}
-            onMouseLeave={toggleSparks}>
-            <p>Sparks Lab - Software Engineer</p>
-            <div 
-              style={{
-                display: sparksExpanded ? true : 'none'
-              }}
-              className='expandableBox'>
-              <p>Led the developement of a React Native device controller application capable of connecting with a research device, running tests, and recieving and processing data to be used in the lab and delivered to Fortune 500 client</p>
-              <p>App usage reduced research testing by 33%</p>
-            </div>
-          </div>
-
-          {/* Viinko */}
-          <div 
-            style={{
-              width: viinkoExpanded ? '80vh' : '55vh',
-              height: viinkoExpanded ? '20vh' : '5vh'
-            }}
-            className='expandable'
-            onMouseEnter={toggleViinko}
-            onMouseLeave={toggleViinko}>
-            <p>Viinko - Software Engineer Intern</p>
-            <div 
-              style={{
-                display: viinkoExpanded ? true : 'none'
-              }}
-              className='expandableBox'>
-              <p>Developed a chrome extension using JavaScript and HTML to be used daily by approximately 24000 students through two schools</p>
-              <p>Chrome extension became a core part of the product allowing students to move their work directly from Google Classroom to Viinko Calendar</p>
-            </div>
-          </div>
+    <div name="experience" className="pageContainer">
+      <div className="contentContainer">
+        <h1 className="titleText">Relevant Experience</h1>
+        <div>
+          <p className="subTitle">Sparks Lab - Software Engineer</p>
+          <ul className="subList">
+            <li className="subListItem">
+              Led the developement of a React Native device controller
+              application capable of connecting with a research device, running
+              tests, and recieving and processing data to be used in the lab and
+              delivered to Fortune 500 client
+            </li>
+            <li className="subListItem">
+              App usage reduced data collection time by 33%
+            </li>
+          </ul>
         </div>
+        <div>
+          <p className="subTitle">Viinko - Software Engineer Intern</p>
+          <ul className="subList">
+            <li className="subListItem">
+              Developed a chrome extension using JavaScript and HTML to be used
+              daily by approximately 2400 students through two schools
+            </li>
+            <li className="subListItem">
+              Chrome extension became a core part of the product allowing
+              students to move their work directly from Google Classroom to
+              Viinko Calendar
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className="subTitle">
+            Business Basics for Engineers - President & Founder
+          </p>
+          <ul className="subList">
+            <li className="subListItem">
+              Founded an academic student organization aimed at teaching myself
+              and my peers how to effectively market their STEM degrees in and
+              out of tech industries. As president I organizaed meetings,
+              recruited speakers from industry, and hosted student led
+              discussions over zoom
+            </li>
+            <li className="subListItem">
+              Maintained a membership of 65 students throughout my tenure
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
