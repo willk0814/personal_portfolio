debf616cb8cbad3e6fdf2f5f03034cbb53a2d144
willk0814 <wkoenig0814@gmail.com>
2023-03-13 16:56:48 -0400

    changes to nodemailer on contact page

--- src/ComponentGroups/Contact/ContactPage.js
+++ src/ComponentGroups/Contact/ContactPage.js
@@ -1,19 +1,87 @@
-import React from 'react'
-import '../PageStyling/PageStyles.css'
+import React, { useState } from "react";
+// import nodemailer from "nodemailer";
+import "../PageStyling/PageStyles.css";
 
 export default function ContactPage() {
+  const [name, setName] = useState("");
+  const [returnEmail, setReturnEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  // const sendEmail = (name, email, message) => {
+  //   const transporter = nodemailer.createTransport({
+  //     service: "gmail",
+  //     auth: {
+  //       user: "wkoenig0814@gmail.com",
+  //       pass: "Walliek0420!!",
+  //     },
+  //   });
+
+  //   const mailOptions = {
+  //     from: email,
+  //     to: "wkoenig0814@gmail.com",
+  //     subject: "PERSONAL PORTFOLIO CONTACT REQUEST",
+  //     text: message,
+  //   };
+
+  //   transporter.sendMail(mailOptions, function (error, info) {
+  //     if (error) {
+  //       console.log(err);
+  //     } else {
+  //       console.log("email sent: ", info.response);
+  //     }
+  //   });
+  // };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // sendEmail(name, returnEmail, message);
+    console.log(
+      "Name: ",
+      name,
+      "Return Email: ",
+      returnEmail,
+      "Message: ",
+      message
+    );
+    setName("");
+    setReturnEmail("");
+    setMessage("");
+  };
+
   return (
-    <div name='contact' className='pageContainer'>
-        <h1 className='titleText'>Contact Me</h1>
-        <form className='contactForm'>
-          <p>Submit the form below or shoot me an email - wkoenig0814@gmail.com</p>
-          <input type='text' placeholder='name' name='name'/> 
-          <br/>
-          <input type='email' placeholder='email' name='email'/>
-          <br/>
-          <textarea name="message" id="" cols="30" rows="10" placeholder='message' />
-          <button className='submitMessageButton'>Submit</button>
-        </form>
+    <div name="contact" className="pageContainer">
+      <h1 className="titleText">Contact Me</h1>
+      <form className="contactForm" onSubmit={handleSubmit}>
+        <p>
+          Submit the form below or shoot me an email - wkoenig0814@gmail.com
+        </p>
+        <input
+          type="name"
+          placeholder="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <br />
+        <input
+          type="email"
+          placeholder="email"
+          value={returnEmail}
+          onChange={(event) => setReturnEmail(event.target.value)}
+        />
+        <br />
+        <textarea
+          name="message"
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="message"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+        <button className="submitMessageButton" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
-  )
+  );
 }
